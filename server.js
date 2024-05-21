d341e3be76d4644d1a8f30ce25df6f116239f3eb
tom mattathil <tsmmattathil@gmail.com>
2024-05-21 11:10:32 +0530

    validator and middlewares

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app.js');
 // console.log(app.get('env'));
 
 // console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
 
 //START SERVER
 const port = process.env.PORT || 3000;
