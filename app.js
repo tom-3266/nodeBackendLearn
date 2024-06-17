fefff8827b7f3a9b69e493ac63791d0670a85352
tom mattathil <tsmmattathil@gmail.com>
2024-06-17 11:23:53 +0530

    added error checks

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -26,5 +27,30 @@ app.use((req, res, next) => {
 //MOUNTING THE ROUTER
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Cannot find ${req.originaUrl} on this server`
+  // });
+  // const err = new Error(
+  //   `Cannot find ${req.originalUrl} on this server`
+  // );
+  // err.statusCode = 404;
+  // err.status = 'fail';
+  next(
+    new AppError(`Cannot find ${req.originalUrl} on this server`, 404)
+  );
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 
 module.exports = app;
